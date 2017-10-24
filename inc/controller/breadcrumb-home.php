827fa10e9920e9660165b165ebcfe014f4358e08
mignonstyle <mignonxstyle@gmail.com>
2017-10-24 13:48:46 +0900

    add home.php

---
+++ inc/controller/breadcrumb-home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Front page and home item of breadcrumbs.
+ *
+ * @package ms-breadcrumb
+ * @author  Mignon Style
+ * @license GPLv2 or later
+ * @version 1.0
+ */
+
+/**
+ * Class MS_Breadcrumb_Home
+ */
+class MS_Breadcrumb_Home extends MS_Breadcrumb_Abstract {
+
+}
