57c1497fdf07856b34bc8012b4dcdb59ee1088a4
mignonstyle <mignonxstyle@gmail.com>
2017-10-24 19:09:14 +0900

    modify post-type-archive

--- inc/controller/breadcrumb-post-type-archive.php
+++ inc/controller/breadcrumb-post-type-archive.php
@@ -19,14 +19,11 @@ class MS_Breadcrumb_Post_Type_Archive extends MS_Breadcrumb_Abstract {
 	 * @return void
 	 */
 	protected function set_items() {
-		$post_type = get_query_var( 'post_type' );
-
-		if ( is_array( $post_type ) ) {
-			$post_type = array_shift( $post_types );
-		}
+		$post_type = $this->get_post_type();
 
 		if ( $post_type && 'post' !== $post_type ) {
 			$label = $this->get_post_type_archive_label( $post_type );
+
 			$this->set( $label );
 		}
 	}
