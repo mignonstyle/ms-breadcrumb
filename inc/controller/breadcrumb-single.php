f968215e86cc26e5a1ee7cd533d41449ad0a5477
mignonstyle <mignonxstyle@gmail.com>
2017-10-24 20:55:58 +0900

    modify single.php function

--- inc/controller/breadcrumb-single.php
+++ inc/controller/breadcrumb-single.php
@@ -19,8 +19,16 @@ class MS_Breadcrumb_Single extends MS_Breadcrumb_Abstract {
 	 * @return void
 	 */
 	protected function set_items() {
-		$post_id  = get_the_ID();
-		$taxonomy = $this->get_post_taxonomy( $post_id );
+		$post_id   = get_the_ID();
+		$post_type = $this->get_post_type();
+		$taxonomy  = $this->get_post_taxonomy( $post_id );
+
+		if ( ! ( 'post' == $post_type ) ) {
+			$label = $this->get_post_type_archive_label( $post_type );
+			$url   = $this->get_post_type_archive_link( $post_type );
+
+			$this->set( $label, $url );
+		}
 
 		if ( $taxonomy ) {
 			$this->set_terms( $post_id, $taxonomy );
