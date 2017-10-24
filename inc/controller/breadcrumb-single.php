d6803d8643974597b75a5550790243b2fdbf879f
mignonstyle <mignonxstyle@gmail.com>
2017-10-24 20:43:54 +0900

    modify single.php function

--- inc/controller/breadcrumb-single.php
+++ inc/controller/breadcrumb-single.php
@@ -19,42 +19,13 @@ class MS_Breadcrumb_Single extends MS_Breadcrumb_Abstract {
 	 * @return void
 	 */
 	protected function set_items() {
-		$taxonomies = get_post_taxonomies();
+		$post_id  = get_the_ID();
+		$taxonomy = $this->get_post_taxonomy( $post_id );
 
-		if ( $taxonomies ) {
-			$taxonomy = array_shift( $taxonomies );
-			$this->set_terms( $taxonomy );
+		if ( $taxonomy ) {
+			$this->set_terms( $post_id, $taxonomy );
 		}
 
 		$this->set( get_the_title() );
-	}
-
-	/**
-	 * Sets Breadcrumbs items of terms or categories.
-	 *
-	 * @param string $taxonomy taxonomy slug.
-	 * @return void
-	 */
-	protected function set_terms( $taxonomy ) {
-		$terms = get_the_terms( get_the_ID(), $taxonomy );
-
-		if ( ! is_array( $terms ) ) {
-			return;
-		}
-
-		$term_key  = key( $terms );
-		$parent_id = 0;
-
-		foreach ( $terms as $key => $object ) {
-
-			if ( 0 < $object->parent && ( 0 === $parent_id || $object->parent === $parent_id ) ) {
-				$term_key  = $key;
-				$parent_id = $object->term_id;
-			}
-		}
-		$term = $terms[ $term_key ];
-
-		$this->set_ancestors( $parent_id, $taxonomy );
-		$this->set( $term->name, get_term_link( $term ) );
 	}
 }
