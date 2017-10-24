8079ba29a17aeb59b413c628edb5b0a08b60d2f2
mignonstyle <mignonxstyle@gmail.com>
2017-10-24 14:53:23 +0900

    add home.php functions

--- inc/controller/breadcrumb-home.php
+++ inc/controller/breadcrumb-home.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Front page and home item of breadcrumbs.
+ * Front page and home item of breadcrumbs
  *
  * @package ms-breadcrumb
  * @author  Mignon Style
@@ -13,4 +13,19 @@
  */
 class MS_Breadcrumb_Home extends MS_Breadcrumb_Abstract {
 
+	/**
+	 * Sets breadcrumbs items
+	 *
+	 * @return void
+	 */
+	protected function set_items() {
+		$label = get_bloginfo( 'name' );
+		$url   = get_home_url();
+
+		if ( is_front_page() || is_home() ) {
+			$this->set( $label );
+		} else {
+			$this->set( $label, $url );
+		}
+	}
 }
