2cb5c4a1fa295d1aa1fc25a7ef41b63ab5b42012
mignonstyle <mignonxstyle@gmail.com>
2017-10-28 21:29:28 +0900

    add option markup

---
+++ inc/options/option-settings/option-markup.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Option page markup.
+ *
+ * @package ms-breadcrumb
+ * @author  Mignon Style
+ * @license GPLv2 or later
+ * @version 1.0
+ */
+
+/**
+ * Class MS_Breadcrumb_Option_Markup.
+ */
+class MS_Breadcrumb_Option_Markup {
+
+	/**
+	 * Constructor Define.
+	 */
+	public function __construct() {
+	}
+
+	/**
+	 * Options page callback. option page markup.
+	 */
+	public function create_option_page() {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( __( 'You do not have sufficient permissions to access this page.', 'ms-breadcrumb' ) );
+		}
+
+		$default = new MS_Breadcrumb_Options_Default();
+		?>
+		<div id="c-ms-breadcrumb" class="wrap">
+			<h2><?php echo esc_html( $default->option_title() ); ?></h2>
+
+			<form method="post" action="options.php">
+				<?php
+					settings_fields( MS_Breadcrumb_Options_Default::OPTION_GROUP );
+					do_settings_sections( MS_Breadcrumb_Options_Default::OPTIONS_PAGE_SLUG );
+					submit_button();
+				?>
+			</form>
+		</div>
+		<?php
+	}
+}
