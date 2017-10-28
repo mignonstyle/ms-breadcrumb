f505fe102204828ad08dc5b1939bb69d5a0a4678
mignonstyle <mignonxstyle@gmail.com>
2017-10-28 21:24:01 +0900

    add admin option scripts

---
+++ inc/options/option-settings/admin-scripts.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Add plugin option page style and scripts.
+ *
+ * @package ms-breadcrumb
+ * @author  Mignon Style
+ * @license GPLv2 or later
+ * @version 1.0
+ */
+
+/**
+ * Class MS_Breadcrumb_Admin_Scripts.
+ */
+class MS_Breadcrumb_Admin_Scripts {
+
+	/**
+	 * Constructor Define.
+	 */
+	public function __construct() {
+	}
+
+	/**
+	 * Read option page style and script.
+	 */
+	public function add_admin_print_scripts() {
+	}
+}
