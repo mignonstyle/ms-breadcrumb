7de4cb87293a1a62e6ebc0a20436c17760ef834e
mignonstyle <mignonxstyle@gmail.com>
2017-11-01 11:05:19 +0900

    modify text-field

--- inc/options/option-fields/field-text.php
+++ inc/options/option-fields/field-text.php
@@ -17,23 +17,21 @@ class MS_Breadcrumb_Field_Text extends MS_Breadcrumb_fields_Abstract {
 	 * Sets option field items.
 	 */
 	protected function set_field() {
-		$fields = $this->get();
-		$class  = empty( $fields['class'] ) ? 'regular-text' : $fields['class'];
+		$field  = $this->get();
+		$legend = $this->get_legend();
+		$label  = $this->get_label();
 
-		if ( isset( $fields['label'] ) && $fields['label'] ) {
-			printf(
-				'<label for="%1$s">%2$s</label>',
-				esc_attr( $fields['fieldname'] ),
-				esc_attr( $fields['label'] )
-			);
-		}
+		$class  = empty( $field['class'] ) ? 'regular-text' : $field['class'];
+		$class .= empty( $this->get_class_field_group() ) ? '' : ' ' . $this->get_class_field_group();
 
 		printf(
-			'<input id="%1$s" name="%1$s" value="%2$s" class="%3$s" type="%4$s" />',
-			esc_attr( $fields['fieldname'] ),
-			esc_attr( $fields['value'] ),
+			'<fieldset>%5$s%6$s<input id="%1$s" name="%1$s" value="%2$s" class="%3$s" type="%4$s" /></fieldset>',
+			esc_attr( $field['fieldname'] ),
+			esc_attr( $field['value'] ),
 			esc_attr( $class ),
-			esc_attr( $fields['type'] )
+			esc_attr( $field['type'] ),
+			$legend,
+			$label
 		);
 	}
 }
